040a7e5b639fac10f7a6601e41b658e3fb0c61d6
Ivan24-wq <iv8747583@gmail.com>
2025-02-26 11:11:03 +0300

    code

--- Груповой проект/auth_module/src/telegram_auth.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include <string>
-using namespace std;
-
-bool authentificationWithtelegram(const string& userId){
-    cout << "Authentificate: " << userId << "withTelegram \n";
-    return true;
-}
-
