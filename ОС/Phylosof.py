038ae3d1c66f116f8e807d3443578906d0a65658
Ivan24-wq <iv8747583@gmail.com>
2025-03-19 11:59:41 +0300

    code

--- ОС/Phylosof.py
+++ ОС/Phylosof.py
@@ -2,16 +2,16 @@ import threading
 import time
 import random
 class Phylofod(threading.Thread):
-    def __init__(sef, name, right_fork, left_fork):
-        dinner().__init__()
+    def __init__(self, name, right_fork, left_fork):
+        super().__init__()
         self.name = name
-        right.fork = right_fork
-        left.fort = left_fork
+        self.left_fork = left_fork
+        self.right_fork = right_fork 
 
     def run(self):
         while True:
             print(f"{self.name} Философ размышляет!")  # Философ "размышляет"
-            time.sleep(random.uniform(1.3))
+            time.sleep(random.uniform(1,3))
 
             #Философ пытается взять вилку
             print(f"{self.name} Попытка взять вилку")
@@ -24,7 +24,17 @@ class Phylofod(threading.Thread):
             print(f"{self.name} Философ взял первуб вилку")
             with second_fork:
                 print(f"{self.name} Философ взфл вторую вилку")
-                time.sleep(random.uniform(1.3))
-                print(f"{salf.name} Философ закончил есть и положит вилки на стол")
+                time.sleep(random.uniform(1,3))
+                print(f"{self.name} Философ закончил есть и положит вилки на стол")
 #Условие задачи
-                
+n = 5
+forks = [threading.Lock() for _ in range(n)]
+philosofs = [
+    Phylofod(f"Философ {n + 1}", forks[i], forks[(i + 1) % n])
+    for i in range(n)
+]
+#Бесконечный цикл
+for philosof in philosofs:
+    philosof.start()
+for philosof in philosofs:
+    philosof.join()    
