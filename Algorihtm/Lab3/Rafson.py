c7036d95d9af4e2a119acc75aedfcf4cba25dfdd
Ivan24-wq <iv8747583@gmail.com>
2025-03-21 13:24:46 +0300

    update code

--- Algorihtm/Lab3/Rafson.py
+++ Algorihtm/Lab3/Rafson.py
@@ -1,20 +1,22 @@
 import numpy as np
 
-def NewtonRafson(F, J, X0, tol=1e-6, max_iteration=1000):
+def NewtonRaphson(F, J, X0, tol=1e-6, max_iteration=1000):
     X = X0
-    for i in range(max_iteration):
+    for _ in range(max_iteration):
         F_value = F(X)
-        J_value = J(X)  # Матрица Якоби
+        J_value = J(X)
+
+        if np.linalg.det(J_value) == 0:
+            raise ValueError("Матрица Якоби вырожденная, невозможно решить систему.")
+
         delta_X = np.linalg.solve(J_value, -F_value)
-
-        
         X = X + delta_X
-
-        # Проверка условия 
+        # Проверка, что изменение решения достаточно маленькое (проверка сходимости)
+        
         if np.linalg.norm(delta_X, ord=np.inf) < tol:
             return X
-    raise ValueError("Недостаточно количества итераций")
 
+    raise ValueError("Метод Ньютона-Рафсона не сошелся за заданное число итераций.")
 
 def F(X):
     x, y = X
@@ -23,9 +25,8 @@ def F(X):
         np.cos(y - 2) + x
     ])
 
-# Определяем матрицу Якоби
 def J(X):
-    x, y = X  
+    x, y = X
     return np.array([
         [np.cos(x + 0.5), -1],
         [1, -np.sin(y - 2)]
@@ -34,9 +35,18 @@ def J(X):
 # Начальное приближение
 X0 = np.array([0.0, 0.0])
 
-# Решение
+# Решение системы
 try:
-    solution = NewtonRafson(F, J, X0)
-    print("Ответ: ", solution)
+    solution = NewtonRaphson(F, J, X0)
+    print("\nОтвет:", solution)
+
+    # Проверка результата
+    F_solution = F(solution)
+    print("\nПроверка решения:")
+    print("F(X):", F_solution)
+    if np.allclose(F_solution, 0, atol=1e-6):
+        print("Решение корректно, значения функций близки к нулю.")
+    else:
+        print("Решение некорректно, значения функций не равны нулю.")
 except ValueError as e:
-    print(e)
+    print("\nОшибка:", e)
