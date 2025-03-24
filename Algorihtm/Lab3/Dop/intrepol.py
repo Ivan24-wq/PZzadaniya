45dd51806d16654d54e1c02421824e21b08519e9
Ivan24-wq <iv8747583@gmail.com>
2025-03-24 20:45:59 +0300

    Interpolation

--- Algorihtm/Lab3/Dop/intrepol.py
+++ Algorihtm/Lab3/Dop/intrepol.py
@@ -1,18 +1,37 @@
 import numpy as np
 
-#Ввод данных
-x0, x1, x2 = map(float, input("Введи через пробел приближенные x0, x1, x2: ").split())
+# Ввод данных
+x0, x1, x2 = map(float, input("Введите через пробел точки x0, x1, x2: ").split())
 
-#Заданная функция
+# Заданная функция
 def Function(x):
-    return (np.log(10)/np.log(x) - 7/(2*x + 6))
-    #Аварийное число
-    if (x==-3):
-        print("Ошибка! На ноль делить нельзя!")
+    if x == -3:  # Аварийное число
+        raise ValueError("Ошибка! На ноль делить нельзя!")
+    return np.log(10) / np.log(x) - 7 / (2 * x + 6)
 
-#Считаем y
+# Считаем значения функции
 y0 = Function(x0)
 y1 = Function(x1)
 y2 = Function(x2)
 
-print(f"y: {y0, y1, y2}")
+# Обратная квадратичная интерполяция
+def Interpolation(x0, x1, x2, y0, y1, y2):
+    # Формула для обратной квадратичной интерполяции
+    denominator = (y0 - y1) * (y0 - y2) * (y1 - y2)
+    
+    # Коэффициенты интерполяционного полинома
+    x_min = (
+        (y1 * y2) / ((y0 - y1) * (y0 - y2)) * x0 +
+        (y0 * y2) / ((y1 - y0) * (y1 - y2)) * x1 +
+        (y0 * y1) / ((y2 - y0) * (y2 - y1)) * x2
+    )
+    
+    return x_min
+
+# Выполнение интерполяции
+x_min = Interpolation(x0, x1, x2, y0, y1, y2)
+y_min = Function(x_min)
+
+# Вывод результатов
+print(f"Значения y: y0 = {y0}, y1 = {y1}, y2 = {y2}")
+print(f"Минимум функции: x = {x_min}, y = {y_min}")
