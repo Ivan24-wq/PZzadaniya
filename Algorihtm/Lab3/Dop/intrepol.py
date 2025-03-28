334cb4b45c5769d99b4648ebbc296c7e3aeb3037
Ivan24-wq <iv8747583@gmail.com>
2025-03-28 11:03:55 +0300

    update

--- Algorihtm/Lab3/Dop/intrepol.py
+++ Algorihtm/Lab3/Dop/intrepol.py
@@ -5,9 +5,7 @@ x0, x1, x2 = map(float, input("Введите через пробел точки
 
 # Заданная функция
 def Function(x):
-    if x == -3:  # Аварийное число
-        raise ValueError("Ошибка! На ноль делить нельзя!")
-    return np.log(10) / np.log(x) - 7 / (2 * x + 6)
+    return np.exp(x) + x**2 -2
 
 # Считаем значения функции
 y0 = Function(x0)
@@ -34,4 +32,4 @@ y_min = Function(x_min)
 
 # Вывод результатов
 print(f"Значения y: y0 = {y0}, y1 = {y1}, y2 = {y2}")
-print(f"Минимум функции: x = {x_min}, y = {y_min}")
+print(f"Минимум функции: x = {x_min:.0f}, y = {y_min:.0f}")
