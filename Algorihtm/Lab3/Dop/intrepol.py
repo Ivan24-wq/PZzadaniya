6945ec2080a6fd9837bebf8d34e00935c61b3524
Ivan24-wq <iv8747583@gmail.com>
2025-03-29 10:35:44 +0300

    Added a check for division by zero and a message output for it.

--- Algorihtm/Lab3/Dop/intrepol.py
+++ Algorihtm/Lab3/Dop/intrepol.py
@@ -5,10 +5,7 @@ x0, x1, x2 = map(float, input("Введите через пробел точки
 
 # Заданная функция
 def Function(x):
-    if x == -3:  # Аварийное число
-        raise ValueError("Ошибка! На ноль делить нельзя!")
-    return np.log(10) / np.log(x) - 7 / (2 * x + 6)
-
+    return x**2
 # Считаем значения функции
 y0 = Function(x0)
 y1 = Function(x1)
@@ -16,8 +13,12 @@ y2 = Function(x2)
 
 # Обратная квадратичная интерполяция
 def Interpolation(x0, x1, x2, y0, y1, y2):
-    # Формула для обратной квадратичной интерполяции
+
+     # Формула для обратной квадратичной интерполяции
     denominator = (y0 - y1) * (y0 - y2) * (y1 - y2)
+    #Добавим проверку деления на 0
+    if denominator == 0:
+        raise ValueError("Деление на ноль!")
     
     # Коэффициенты интерполяционного полинома
     x_min = (
@@ -29,9 +30,13 @@ def Interpolation(x0, x1, x2, y0, y1, y2):
     return x_min
 
 # Выполнение интерполяции
-x_min = Interpolation(x0, x1, x2, y0, y1, y2)
-y_min = Function(x_min)
-
-# Вывод результатов
-print(f"Значения y: y0 = {y0}, y1 = {y1}, y2 = {y2}")
-print(f"Минимум функции: x = {x_min}, y = {y_min}")
+try:
+    x_min = Interpolation(x0, x1, x2, y0, y1, y2)
+    y_min = Function(x_min)
+    # Вывод результатов
+    print(f"Значения y: y0 = {y0}, y1 = {y1}, y2 = {y2}")
+    print(f"Минимум функции: x = {x_min:.1f}, y = {y_min:.1f}")
+except ValueError as e:
+    print(f"Ошибка! Повторите ввод данных {e}")
+except ValueError as e:
+    print(f"Деление на ноль! {e}")
