1f58c865f530eeaf57cea5e6474e6157f1135359
Ivan24-wq <iv8747583@gmail.com>
2025-04-29 10:58:57 +0300

    code

--- Algorihtm/Lab6/zadazha.py
+++ Algorihtm/Lab6/zadazha.py
@@ -2,14 +2,20 @@ import pulp
 from pult import LpProblem, LpMaximize, LpVariable, LpInteger
 from fractions import Fraction
 
-prob = LpProblem("Метод Ветвей и границ: ", pulp.LpMaximize)
+def zadacha(branch = None):
+    prob = LpProblem("Метод Ветвей и границ: ", pulp.LpMaximize)
 
-x1 = LpVariable("x1", lowBound=0, cat="Integer")
-x2=LpVariable("x2", lowBound=0, cat="Integer")
+    x1 = LpVariable("x1", lowBound=0, cat="Integer")
+    x2=LpVariable("x2", lowBound=0, cat="Integer")
 
-#Целевая функция
-prob += 2 * x1 + 3 * x2
+    #Целевая функция
+    prob += 2 * x1 + 3 * x2
 
-#Ограничения
-prob += 2 * x1 + x2 <=Fraction(19, 3)
-prob += x1 + 3 * x2 <= 10
+    #Ограничения
+    prob += 2 * x1 + x2 <=Fraction(19, 3)
+    prob += x1 + 3 * x2 <= 10
+
+    #Ветвление
+    if branch:
+        for constraint in branch:
+            prob += constraint
